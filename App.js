dbd0b24d3b92f265968a4e7486d7fd339f1a46d6
V45QU3Z <alcibarvasquez@gmail.com>
2020-09-18 09:49:04 -0500

    version updated

--- App.js
+++ App.js
@@ -4,10 +4,7 @@ import React, { useState } from 'react';
 import Login from './app/screens/login/Login';
 import Navigation from './app/screens/drawer/navigation/Nagivation';
 import { View } from 'react-native';
-import SnackBar from './app/components/SnackBar'
-import SplashScreen from './app/components/SplashScreen'
-import SnackBarr from './app/components/SnackBarr';
-import TabScreen from './app/screens/drawer/stacks/TabStack';
+
 
 export default function App() {
 
