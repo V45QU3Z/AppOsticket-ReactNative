cabdbfeb2503981d8a84e5832b3b8877eece57c3
V45QU3Z <alcibarvasquez@gmail.com>
2020-09-18 09:50:50 -0500

    changes updated

--- App.js
+++ App.js
@@ -1,9 +1,7 @@
 import 'react-native-gesture-handler';
 
 import React, { useState } from 'react';
-import Login from './app/screens/login/Login';
 import Navigation from './app/screens/drawer/navigation/Nagivation';
-import { View } from 'react-native';
 
 
 export default function App() {
